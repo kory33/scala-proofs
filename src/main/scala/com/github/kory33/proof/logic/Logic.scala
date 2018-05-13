45c6585b245207454f8872e2a89c75401e8d08e7
kory33 <6561358+kory33@users.noreply.github.com>
2018-05-13 17:36:16 +0800

    prove basic theorems in propositional logic

---
+++ src/main/scala/com/github/kory33/proof/logic/Logic.scala
@@ -0,0 +1,118 @@
+package com.github.kory33.proof.logic
+
+object Logic {
+
+  type ￢[A] = A => Nothing
+  type ∧[A, B] = (A, B)
+  type ∨[A, B] = Either[A, B]
+  type <=[A, B] = B => A
+
+  implicit class Theorem[A](instance: A) {
+    def ∧[B](b: B): A ∧ B = (instance, b)
+  }
+
+  def identity[A]: A => A = { theorem: A => theorem }
+
+  /**
+    * Modus-Ponens
+    */
+  def MP[A, B]: (A, A => B) => B = { (a, deduction) => deduction(a) }
+
+  /**
+    * Proof by contradiction
+    */
+  implicit def byContradiction[A]: (A => Nothing) => ￢[A] = identity
+
+  /**
+    * separation
+    */
+  implicit def sep[A, B]: A ∧ B => A = { conj => conj._1 }
+
+  /**
+    * Disjunctions
+    */
+  implicit def leftDisj[A, B](a: A): A ∨ B = Left(a)
+  implicit def rightDisj[A, B](b: B): A ∨ B = Right(b)
+
+  /**
+    * removal of disjunction
+    */
+  def removeDisj[A, B, C](): (A ∨ B, A => C, B => C) => C = { (disj, deduc1, deduc2) =>
+    disj match {
+      case Left(a) => deduc1(a)
+      case Right(b) => deduc2(b)
+    }
+  }
+
+  implicit def contradiction[A]: A ∧ ￢[A] => Nothing = { case (a, notA) => notA(a) }
+
+  def explosion[A]: Nothing => A = _ => ???
+
+  implicit def commuteDisj[A, B]: A ∨ B => B ∨ A = { conj => conj.swap }
+  implicit def commuteConj[A, B]: A ∧ B => B ∧ A = { case (a, b) => (b, a) }
+
+  /**
+    * law of noncontradiction
+    */
+  def noncontradiction: ￢[Nothing] = identity
+
+  /**
+    * Modus tollens
+    */
+  def MT[A, B]: (A => B) ∧ ￢[B] => ￢[A] = { case (ded, notB) =>
+    val contradictory: A => Nothing = { a: A => (ded(a), notB) }
+    byContradiction(contradictory)
+  }
+
+  def contraposition[A, B]: (A => B) => (￢[B] => ￢[A]) = { ded => { notB => MT(ded ∧ notB) } }
+
+  /**
+    * Disjunctive syllogism / modus tollendo ponens
+    */
+  def MTP[A, B]: ((A ∨ B) ∧ ￢[A]) => B = { case(disj, notA) =>
+      disj match {
+        case Left(a) => explosion(a ∧ notA)
+        case Right(b) => b
+      }
+  }
+
+  implicit class ≣[A, B](ev: (A => B) ∧ (B => A)) {
+    def implies: A => B = ev._1
+    def impliedBy: B => A = ev._2
+  }
+
+  def deMorgan[A, B]: ￢[A ∨ B] ≣ (￢[A] ∧ ￢[B]) = {
+    val implies: ￢[A ∨ B] => (￢[A] ∧ ￢[B]) = { contradictory: (A ∨ B => Nothing) =>
+      val notA = { a: A => contradictory(a) }
+      val notB = { b: B => contradictory(b) }
+      notA ∧ notB
+    }
+    val impliedBy: ￢[A ∨ B] <= (￢[A] ∧ ￢[B]) = { case(notA, notB) =>
+        val contradictory: A ∨ B => Nothing = {
+          case Left(a) => a ∧ notA
+          case Right(b) => b ∧ notB
+        }
+        byContradiction(contradictory)
+    }
+
+    implies ∧ impliedBy
+  }
+
+  def transitive[A, B, C]: ((A => B) ∧ (B => C)) => A => C = { case(f, g) => g compose f }
+
+  def distributive[A, B, C]: (A ∧ (B ∨ C)) ≣ ((A ∧ B) ∨ (A ∧ C)) = {
+    val implies: (A ∧ (B ∨ C)) => ((A ∧ B) ∨ (A ∧ C)) = { case(a, bOrC) =>
+      bOrC match {
+        case Left(b) => a ∧ b
+        case Right(c) => a ∧ c
+      }
+    }
+    val impliedBy: (A ∧ (B ∨ C)) <= ((A ∧ B) ∨ (A ∧ C)) = {
+      case Left((a, b)) => a ∧ b
+      case Right((a, c)) => a ∧ c
+    }
+
+    implies ∧ impliedBy
+  }
+
+}
