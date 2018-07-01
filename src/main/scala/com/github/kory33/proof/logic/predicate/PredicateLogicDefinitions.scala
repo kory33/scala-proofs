7823afe9a95d8b63805e1e2c9ee9e0ae3a9ee746
kory33 <6561358+kory33@users.noreply.github.com>
2018-07-02 00:21:52 +0800

    fix definition of quantifiers and prove existential elim/gen

--- src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicDefinitions.scala
+++ src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicDefinitions.scala
@@ -4,8 +4,11 @@ import com.github.kory33.proof.logic.propositional.LogicDefinitions.￢
 
 object PredicateLogicDefinitions {
 
-  type ∀[F[_]] = ￢[∃[[x] => ￢[F[x]]]]
+  trait ∃[F[_]] {
+    type S
+    def value: F[S]
+  }
 
-  type ∃[F[_]] = F[_]
+  type ∀[F[_]] = ￢[∃[[x] => ￢[F[x]]]]
 
 }
--- src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicSystem.scala
+++ src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicSystem.scala
@@ -10,40 +10,23 @@ object PredicateLogicSystem {
   /**
     * Existential generalization
     */
-  implicit def genExist[F[_], A](instance: F[A]): ∃[F] = instance.asInstanceOf[F[_]]
+  implicit def genExist[F[_], A](instance: F[A]): ∃[F] = new ∃ { type S = A; def value = instance }
 
-/*
   /**
     * Existential instantiation(elimination)
     */
   def instExist[F[_], φ](exists: ∃[F], forall: ∀[[X] => F[X] => φ])(implicit axiom: PredicateLogicAxiom): φ = {
-    axiom.instUniv[[X] => F[X] => φ](forall)
+    axiom.instUniv[[X] => F[X] => φ, exists.S](forall)(exists.value)
   }
 
-  /*
   /**
     * ∀x.∀y.F(x, y) ⇔ ∀y.∀x.F(x, y)
     */
-  def forallCommute[F[_, _]]: ∀[[x] => ∀[[y] => F[x, y]]] ≣ ∀[[y] => ∀[[x] => F[x, y]]] = {
-
-    val implies:
-      ∀[({type λ1[x] = ∀[({ type λ2[y] = F[x, y] })#λ2] })#λ1] =>
-        ∀[({type λ1[y] = ∀[({ type λ2[x] = F[x, y] })#λ2] })#λ1] = identity
-
-    val impliedBy:
-      ∀[({type λ1[y] = ∀[({ type λ2[x] = F[x, y] })#λ2] })#λ1] =>
-        ∀[({type λ1[x] = ∀[({ type λ2[y] = F[x, y] })#λ2] })#λ1] = identity
-
-    implies ∧ impliedBy
-
-  }
+  def forallCommute[F[_, _]]: ∀[[x] => ∀[[y] => F[x, y]]] ≣ ∀[[y] => ∀[[x] => F[x, y]]] = ??? // TODO
 
   /**
     * ∃x.∃y.F(x, y) ⇔ ∃y.∃x.F(x, y)
     */
-  def existsCommute[F[_, _]]: ∃[[x] => ∃[[y] => F[x, y]]] ≣ ∃[[y] => ∃[[x] => F[x, y]]] = {
+  def existsCommute[F[_, _]]: ∃[[x] => ∃[[y] => F[x, y]]] ≣ ∃[[y] => ∃[[x] => F[x, y]]] = ??? // TODO
 
-  }
-
-  */
 }
