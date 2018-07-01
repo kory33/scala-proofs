9f406b320898854c67042935ac3240a99da48ad3
kory33 <6561358+kory33@users.noreply.github.com>
2018-07-01 16:02:53 +0800

    fix definition of predicate logic symbols

--- src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicDefinitions.scala
+++ src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicDefinitions.scala
@@ -4,14 +4,8 @@ import com.github.kory33.proof.logic.propositional.LogicDefinitions.￢
 
 object PredicateLogicDefinitions {
 
-  type ∀[F[_]] = F[_]
+  type ∀[F[_]] = ￢[∃[({ type λ[A] = ￢[F[A]] })#λ]]
 
-  type ∃[F[_]] = ￢[∀[({ type λ[A] = ￢[F[A]] })#λ]]
-
-  /* With type lambdas
-
-  type ∃[F[_]] = ￢[∀[X :-> ￢[F[X]]]]
-
-   */
+  type ∃[F[_]] = F[_]
 
 }
