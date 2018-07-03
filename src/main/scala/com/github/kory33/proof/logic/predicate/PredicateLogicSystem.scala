23f9e3794a840f787b1358f5c78be0d6560cd9f9
kory33 <6561358+kory33@users.noreply.github.com>
2018-07-04 03:02:41 +0800

    simplify proof of commutativity of quantifiers

--- src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicSystem.scala
+++ src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLogicSystem.scala
@@ -28,29 +28,18 @@ object PredicateLogicSystem {
     * ∃x.∃y.F(x, y) ⇔ ∃y.∃x.F(x, y)
     */
   def existsCommute[F[_, _]]: ∃[[x] => ∃[[y] => F[x, y]]] ≣ ∃[[y] => ∃[[x] => F[x, y]]] = {
-    val implies: ∃[[x] => ∃[[y] => F[x, y]]] => ∃[[w] => ∃[[z] => F[z, w]]] = { exists =>
+    def implies[G[_, _]]: ∃[[x] => ∃[[y] => G[x, y]]] => ∃[[w] => ∃[[z] => G[z, w]]] = { exists =>
       type X = exists.S
-      val ev1: ∃[[y] => F[X, y]] = exists.value
+      val ev1: ∃[[y] => G[X, y]] = exists.value
 
       type Y = ev1.S
-      val ev2: F[X, Y] = ev1.value
+      val ev2: G[X, Y] = ev1.value
 
-      val ev3: ∃[[x] => F[x, Y]] = genExist[[x] => F[x, Y], X](ev2)
-      genExist[[y] => ∃[[x] => F[x, y]], Y](ev3)
+      val ev3: ∃[[x] => G[x, Y]] = genExist[[x] => G[x, Y], X](ev2)
+      genExist[[y] => ∃[[x] => G[x, y]], Y](ev3)
     }
 
-    val impliedBy: ∃[[y] => ∃[[x] => F[x, y]]] => ∃[[x] => ∃[[y] => F[x, y]]] = { exists =>
-      type Y = exists.S
-      val ev1: ∃[[x] => F[x, Y]] = exists.value
-
-      type X = ev1.S
-      val ev2: F[X, Y] = ev1.value
-
-      val ev3: ∃[[y] => F[X, y]] = genExist[[y] => F[X, y], Y](ev2)
-      genExist[[x] => ∃[[y] => F[x, y]], X](ev3)
-    }
-
-    implies ∧ impliedBy
+    implies[F] ∧ implies[[X, Y] => F[Y, X]]
   }
 
   /**
@@ -59,43 +48,25 @@ object PredicateLogicSystem {
   def forallCommute[F[_, _]](implicit
                              predicateLogicAxiom: PredicateLogicAxiom,
                              classicalLogicAxiom: ClassicalLogicAxiom): ∀[[x] => ∀[[y] => F[x, y]]] ≣ ∀[[y] => ∀[[x] => F[x, y]]] = {
-    val implies = eliminateDoubleNegation(
-      byContradiction { negation: ￢[∀[[x] => ∀[[y] => F[x, y]]] => ∀[[y] => ∀[[x] => F[x, y]]]] =>
+    def implies[G[_, _]] = eliminateDoubleNegation(
+      byContradiction { negation: ￢[∀[[x] => ∀[[y] => G[x, y]]] => ∀[[y] => ∀[[x] => G[x, y]]]] =>
         val ev1 = nonImplication.implies(negation)
-        val ev2: ∀[[x] => ∀[[y] => F[x, y]]] = ev1._1
-        val ev3: ￢[∀[[y] => ∀[[x] => F[x, y]]]] = ev1._2
-        val ev4: ∃[[y] => ￢[∀[[x] => F[x, y]]]] = notForall[[y] => ∀[[x] => F[x, y]]](ev3)
+        val ev2: ∀[[x] => ∀[[y] => G[x, y]]] = ev1._1
+        val ev3: ￢[∀[[y] => ∀[[x] => G[x, y]]]] = ev1._2
+        val ev4: ∃[[y] => ￢[∀[[x] => G[x, y]]]] = notForall[[y] => ∀[[x] => G[x, y]]](ev3)
         type Y = ev4.S
-        val ev5: ￢[∀[[x] => F[x, Y]]] = ev4.value
-        val ev6: ∃[[x] => ￢[F[x, Y]]] = notForall[[x] => F[x, Y]](ev5)
+        val ev5: ￢[∀[[x] => G[x, Y]]] = ev4.value
+        val ev6: ∃[[x] => ￢[G[x, Y]]] = notForall[[x] => G[x, Y]](ev5)
         type X = ev6.S
-        val ev7: ￢[F[X, Y]] = ev6.value
-        val ev8: F[X, Y] = predicateLogicAxiom.instUniv[[y] => F[X, y], Y](
-          predicateLogicAxiom.instUniv[[x] => ∀[[y] => F[x, y]], X](ev2)
+        val ev7: ￢[G[X, Y]] = ev6.value
+        val ev8: G[X, Y] = predicateLogicAxiom.instUniv[[y] => G[X, y], Y](
+          predicateLogicAxiom.instUniv[[x] => ∀[[y] => G[x, y]], X](ev2)
         )
         ev8 ∧ ev7
       }
     )
 
-    val impliedBy = eliminateDoubleNegation(
-      byContradiction { negation: ￢[∀[[y] => ∀[[x] => F[x, y]]] => ∀[[x] => ∀[[y] => F[x, y]]]] =>
-        val ev1 = nonImplication.implies(negation)
-        val ev2: ∀[[y] => ∀[[x] => F[x, y]]] = ev1._1
-        val ev3: ￢[∀[[x] => ∀[[y] => F[x, y]]]] = ev1._2
-        val ev4: ∃[[x] => ￢[∀[[y] => F[x, y]]]] = notForall[[x] => ∀[[y] => F[x, y]]](ev3)
-        type X = ev4.S
-        val ev5: ￢[∀[[y] => F[X, y]]] = ev4.value
-        val ev6: ∃[[y] => ￢[F[X, y]]] = notForall[[y] => F[X, y]](ev5)
-        type Y = ev6.S
-        val ev7: ￢[F[X, Y]] = ev6.value
-        val ev8: F[X, Y] = predicateLogicAxiom.instUniv[[x] => F[x, Y], X](
-          predicateLogicAxiom.instUniv[[y] => ∀[[x] => F[x, y]], Y](ev2)
-        )
-        ev8 ∧ ev7
-      }
-    )
-
-    implies ∧ impliedBy
+    implies[F] ∧ implies[[X, Y] => F[Y, X]]
   }
 
 }
