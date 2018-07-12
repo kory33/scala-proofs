b1fbac8425479294fa4a33792df0bb44eb2392ce
kory33 <6561358+kory33@users.noreply.github.com>
2018-07-12 22:07:48 +0800

    modify axiom of extensinoality to be equivalence rather than implication

--- src/main/scala/com/github/kory33/proof/set/ZFAxiom.scala
+++ src/main/scala/com/github/kory33/proof/set/ZFAxiom.scala
@@ -20,7 +20,7 @@ trait ZFAxiom {
     *
     * For all set x and y, x contains and is contained in y when they have exactly same elements.
     */
-  def extensionality: ∀[[x] => ∀[[y] => ∀[[z] => (z ∈ x) ≣ (z ∈ y)] => x =#= y]]
+  def extensionality: ∀[[x] => ∀[[y] => ∀[[z] => (z ∈ x) ≣ (z ∈ y)] ≣ x =#= y]]
 
   /**
     * Schema of separation.
@@ -78,7 +78,7 @@ trait ZFAxiom {
 
 object ZFAxiom {
   def existence(implicit axiom: ZFAxiom): ∃[[x] => x =#= x] = axiom.existence
-  def extensionality(implicit axiom: ZFAxiom): ∀[[x] => ∀[[y] => ∀[[z] => (z ∈ x) ≣ (z ∈ y)] => x =#= y]] = axiom.extensionality
+  def extensionality(implicit axiom: ZFAxiom): ∀[[x] => ∀[[y] => ∀[[z] => (z ∈ x) ≣ (z ∈ y)] ≣ x =#= y]] = axiom.extensionality
   def separation[F[_, _]](implicit axiom: ZFAxiom): ∀[[x] => ∀[[p] => ∃[[y] => ∀[[u] => (u ∈ y) ≣ ((u ∈ x) ∧ F[u, p])]]]] = axiom.separation
   def pairing(implicit axiom: ZFAxiom): ∀[[a] => ∀[[b] => ∃[[x] => (a ∈ x) ∧ (a ∈ x)]]] = axiom.pairing
   def union(implicit axiom: ZFAxiom): ∀[[F] => ∃[[u] => ∀[[y] => ∀[[x] => ((x ∈ y) ∧ (y ∈ F)) => x ∈ u]]]] = axiom.union
