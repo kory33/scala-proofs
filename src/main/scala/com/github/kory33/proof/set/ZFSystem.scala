fe2b09fb0cb495be0ba3e9023d707efce4ef1625
kory33 <6561358+kory33@users.noreply.github.com>
2018-07-12 22:14:09 +0800

    prove there is no set of all sets

--- src/main/scala/com/github/kory33/proof/set/ZFSystem.scala
+++ src/main/scala/com/github/kory33/proof/set/ZFSystem.scala
@@ -35,4 +35,38 @@ object ZFSystem {
   
     forType[EmptySet].generalize[[x] => ∀[[y] => y ∉ x]](ev3)
   }
+
+  /**
+   * There is no set of all sets.
+   */
+  def noSetOfAllSets(implicit axiom: ZFAxiom): ￢[∃[[x] => ∀[[y] => y ∈ x]]] = {
+    def lemma1[A, B]: ￢[(A ≣ (B ∧ ￢[A])) ∧ B] = byContradiction { assumption: (A ≣ (B ∧ ￢[A])) ∧ B =>
+      val (aEqBAndNotA, b) = assumption
+      def ev1: ￢[A] => A = { notA: ￢[A] => aEqBAndNotA.impliedBy(b ∧ notA) }
+      def ev2: A => ￢[A] = { a: A => aEqBAndNotA.implies(a)._2 }
+      val notAEqNotA = byContradiction { aEqNotA: A ≣ ￢[A] =>
+        val notA = byContradiction { a: A => a ∧ aEqNotA.implies(a) }
+        val a: A = aEqNotA.impliedBy(notA)
+        a ∧ notA
+      }
+      notAEqNotA(ev2 ∧ ev1)
+    }
+
+    byContradiction { assumption: ∃[[x] => ∀[[y] => y ∈ x]] =>
+      type S = assumption.S
+      val setOfAllSets = assumption.value
+
+      val paradoxicalExistence: ∃[[z] => ∀[[u] => (u ∈ z) ≣ ((u ∈ S) ∧ (u ∉ u))]] = 
+        forType[Nothing].instantiate[[p] => ∃[[y] => ∀[[u] => (u ∈ y) ≣ ((u ∈ S) ∧ (u ∉ u))]]](
+          forType[S].instantiate[[x] => ∀[[p] => ∃[[y] => ∀[[u] => (u ∈ y) ≣ ((u ∈ x) ∧ (u ∉ u))]]]](
+            separation[[X, _] => X ∉ X]
+          )
+        )
+      type Z = paradoxicalExistence.S
+      val paradoxicalSet: ∀[[u] => (u ∈ Z) ≣ ((u ∈ S) ∧ (u ∉ u))] = paradoxicalExistence.value
+      val ev1: (Z ∈ Z) ≣ ((Z ∈ S) ∧ (Z ∉ Z)) = forType[Z].instantiate[[u] => (u ∈ Z) ≣ ((u ∈ S) ∧ (u ∉ u))](paradoxicalSet)
+      val ev2: Z ∈ S = forType[Z].instantiate[[y] => y ∈ S](setOfAllSets)
+      lemma1(ev1 ∧ ev2)
+    }
+  }
 }
