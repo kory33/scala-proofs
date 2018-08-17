d2b9a8afd7b9c68ee86adefe9c0c74bdcdcc9fb9
kory33 <6561358+kory33@users.noreply.github.com>
2018-08-17 12:09:46 +0800

    enrich =::=

--- src/main/scala/com/github/kory33/proof/set/logic/Equality.scala
+++ src/main/scala/com/github/kory33/proof/set/logic/Equality.scala
@@ -6,14 +6,12 @@ import com.github.kory33.proof.set.Σ
 @implicitNotFound(msg = "Cannot prove that ${A} =::= ${B}.")
 trait =::=[A <: Σ, B <: Σ] {
   def sub[F[_ <: Σ]]: F[A] => F[B]
+  def commute: B =::= A
 }
 
 object =::= {
   implicit def constructEquality[A <: Σ]: =::=[A, A] = new =::=[A, A] {
     def sub[F[_ <: Σ]]: F[A] => F[A] = identity
-  }
-
-  implicit class SubstitutablePredicate[F[_ <: Σ], A <: Σ](fa: F[A]) {
-    def sub[B <: Σ](implicit ev: =::=[A, B]): F[B] = ev.sub[F](fa)
+    def commute: A =::= A = this
   }
 }
