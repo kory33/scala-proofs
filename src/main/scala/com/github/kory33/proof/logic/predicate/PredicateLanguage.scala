103a6663fae271b0a28723605f4acdf26744b30c
kory33 <6561358+kory33@users.noreply.github.com>
2019-01-08 04:11:05 +0800

    define languages for predicate logic and set theory

--- src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLanguage.scala
+++ src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLanguage.scala
@@ -2,13 +2,36 @@ package com.github.kory33.proof.logic.predicate
 
 import com.github.kory33.proof.logic.propositional.LogicDefinitions._
 
+/**
+ * Language universe of a predicate logic system.
+ */
 trait PredicateLanguage {
     type Obj <: AnyRef
 
-    trait Exists[F[_]] {
-        val witness: Obj
+    trait Term[x <: AnyRef]
+    implicit def objIsTerm(implicit x: Obj): Term[x.type]
+
+    trait ∃[F[_]] {
+        val witness: AnyRef
+        val term: Term[witness.type]
         val proof: F[witness.type]
     }
 
-    type Forall[F[_]] = (Obj => Any) { def apply(x: Obj): F[x.type] }
+    type ∀[F[_]] = { def apply(x: AnyRef)(implicit t: Term[x.type]): F[x.type] }
+}
+
+/**
+ * Language universe of a predicate logic system with equality predicate.
+ */
+trait EqPredLanguage extends PredicateLanguage {
+    trait =::=[A, B] {
+        def sub[F[_]]: F[A] => F[B]
+        def commute: B =::= A = sub[λ[a => a =::= A]](implicitly[A =::= A])
+        def andThen[C](bEqC: B =::= C): A =::= C = bEqC.sub[λ[b => A =::= b]](this)
+    }
+    implicit def typeEq[A]: A =::= A = new =::=[A, A] {
+        override def sub[F[_]] = identity
+        override def commute = this
+        override def andThen[C](aEqC: A =::= C) = aEqC
+    }
 }
---
+++ src/main/scala/com/github/kory33/proof/set/foundation/SetLanguage.scala
@@ -0,0 +1,7 @@
+package com.github.kory33.proof.set.foundation
+
+import com.github.kory33.proof.logic.predicate._
+
+trait SetLanguage extends EqPredLanguage {
+    type ∈[_, _]
+}
