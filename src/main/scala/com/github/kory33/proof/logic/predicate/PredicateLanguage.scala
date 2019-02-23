b119a2ecf0e5475cf3861450e8316cd681fe33d3
kory33 <6561358+kory33@users.noreply.github.com>
2019-02-23 23:04:46 +0800

    use epsilon operator for defining function symbols

--- src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLanguage.scala
+++ src/main/scala/com/github/kory33/proof/logic/predicate/PredicateLanguage.scala
@@ -43,23 +43,27 @@ trait EqPredLanguage extends PredicateLanguage {
   implicit def typeEq[A]: A =::= A = new =::=[A, A] {
     override def sub[F[_]] = identity
     override def commute = this
-    override def andThen[C](aEqC: A =::= C) = aEqC
-    override def cast(a: A): A = a
   }
 
   type ∃![F[_]] = ∃[F] ∧ ∀[[x] => ∀[[y] => (F[x] ∧ F[y]) => x =::= y]]
+}
 
-  def isomorphism[P[_]: ∃!](ex: ∃[P]): ex.witness.type =::= ∃[P]#W
-  implicit def isomorphismTerm[P[_]](ex: ∃![P]): Univ[∃[P]#W]
+trait EpsilonCalculus extends EqPredLanguage {
+  type ε[P[_]]
+  def epsilon[P[_]: ∃]: ε[P]
+  def epsilonPred[P[_]](x: ε[P]): P[x.type]
+  implicit def epsilonUniv[P[_]](implicit x: ε[P]): Univ[x.type]
+  def epsilonUniqueness[P[_]: ∃!](x: ε[P]): x.type =::= ε[P]
 
   // Identity functional symbol
-  type Id[z] = ∃[[x] => z =::= x]#W
+  type Id = [z] => ε[[x] => z =::= x]
 
   object Id {
+    def existsId(x: Any)(implicit xu: Univ[x.type]): ∃[[y] => x.type =::= y] = genExist(x)
+
     val uniqueness: ∀[[z] => ∃![[x] => z =::= x]] = {
       z: Any => implicit zTerm: Univ[z.type] =>
       type F[x] = z.type =::= x
-      val existence: ∃[[x] => z.type =::= x] = genExist(z)
       val uniquenss: ∀[[x] => ∀[[y] => ((z.type =::= x) ∧ (z.type =::= y)) => x =::= y]] = {
         x: Any => implicit xTerm: Univ[x.type] => y: Any => implicit yTerm: Univ[y.type] => {
           xEqyAndXEqZ: F[x.type] ∧ F[y.type] => {
@@ -67,18 +71,21 @@ trait EqPredLanguage extends PredicateLanguage {
           }
         }
       }
-      existence ∧ uniquenss
-    }
-    val identity: ∀[[x] => x =::= Id[x]] = { z: Any => implicit zTerm: Univ[z.type] =>
-      type F[x] = z.type =::= x
-      implicit val ev1: ∃![[x] => z.type =::= x] = uniqueness(z)
-      val ev2: z.type =::= ev1._1.witness.type = ev1._1.proof
-      isomorphism(ev1._1).sub[[idx] => z.type =::= idx](ev2)
+      existsId(z) ∧ uniquenss
     }
 
-    def id(x: Any)(implicit xu: Univ[x.type]): Id[x.type] = {
-      implicit val ev1: ∃![[y] => x.type =::= y] = uniqueness(x)
-      isomorphism(ev1._1).cast(ev1._1.witness)
+    val identity: ∀[[x] => x =::= Id[x]] = { implicit z: Any => implicit zTerm: Univ[z.type] =>
+      type F[x] = z.type =::= x
+      implicit val idz: ε[F] = epsilon[F](genExist(z))
+      implicit val uniquenessEq: ∃![[x] => z.type =::= x] = uniqueness(z)
+      epsilonUniqueness[F](idz).sub(epsilonPred(idz))
+    }
+
+    def id(x: Any)(implicit xUniv: Univ[x.type]): Id[x.type] = epsilon(existsId(x))
+
+    def idid(x: Any)(implicit xUniv: Univ[x.type]): Id[Id[x.type]] = {
+      implicit val idx: Id[x.type] = id(x)
+      epsilonUniqueness(idx)(uniqueness(x)).sub(id(idx))
     }
   }
 }
